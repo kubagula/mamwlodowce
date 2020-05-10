06d3fe2094441f6253d19bf33a41a91fc5cbe48a
kubagula <almalicka@gmail.com>
2020-05-10 19:32:34 +0200

    add active menu in admin panel

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,28 +1,27 @@
-@if(auth()->user()->is_admin == 1)   
-    <ul class="nav justify-content-center">
-        <li class="nav-item">
-            <a class="nav-link active" href="{{url('admin/ingredients')}}">Składniki</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/recipes')}}">Przepisy</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/categories')}}">Kategorie przepisów</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/types')}}">Typy składników</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/units')}}">Jednostki miar</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/months')}}">Miesiące</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/preservatives')}}">Sztuczne dodatki</a>
-        </li>                          
-    </ul>                                     
+@if(auth()->user()->is_admin == 1)
+<ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link {{ (request()->is('admin/ingredients*')) ? 'active' : '' }}" href="{{url('admin/ingredients')}}">Składniki</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ (request()->is('admin/recipes*')) ? 'active' : '' }}" href="{{url('admin/recipes')}}">Przepisy</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ (request()->is('admin/categories*')) ? 'active' : '' }}" href="{{url('admin/categories')}}">Kategorie przepisów</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ (request()->is('admin/types*')) ? 'active' : '' }}" href="{{url('admin/types')}}">Typy składników</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ (request()->is('admin/units*')) ? 'active' : '' }}" href="{{url('admin/units')}}">Jednostki miar</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ (request()->is('admin/months*')) ? 'active' : '' }}" href="{{url('admin/months')}}">Miesiące</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ (request()->is('admin/preservatives*')) ? 'active' : '' }}" href="{{url('admin/preservatives')}}">Sztuczne dodatki</a>
+    </li>
+</ul>
 @else
-    <div class=”panel-heading”>Normal User</div>
+<div class=”panel-heading”>Normal User</div>
 @endif
-      
